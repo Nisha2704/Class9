869f5f1c408b9b90aaaa0db6a325786bd31d4f40
Nisha2704 <68813245+Nisha2704@users.noreply.github.com>
2022-01-02 14:52:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var ball
+
+function setup() {
+  createCanvas(400,400);
+
+  ball = createSprite(200,200,30,40)
+  ball.shapeColor="red"
+}
+
+function draw() 
+{
+  background(30);
+
+  drawSprites()
+
+}
+
+
+
+
